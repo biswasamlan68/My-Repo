1d82cf4394b3a0799aa65445cae2a220594c15af
Amlan <Amlan@LAPTOP-2QK8M9KD>
2021-04-03 11:26:35 +0530

    First Commit

---
+++ GitUpload/src/main/java/print/git.java
@@ -0,0 +1,14 @@
+package print;
+
+public class git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("Git Upload and Jenkins Integration");
+		
+		
+		
+	}
+
+}
