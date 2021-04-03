c26b27a3b6eea513888d0a55b6961d354c811b7c
Amlan <Amlan@LAPTOP-2QK8M9KD>
2021-04-03 12:13:22 +0530

    dfdafsf

--- GitUpload/src/main/java/print/git.java
+++ GitUpload/src/main/java/print/git.java
@@ -5,7 +5,7 @@ public class git {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	System.out.println("Git Upload and Jenkins Integration Test");
+	System.out.println("Git Upload and Jenkinsdsfd Integration Test");
 		
 		
 		
